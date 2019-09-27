7656626d583a7a68d71328c8bfb52862bd99b985
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-09-28 00:58:30 +0300

    Updates

---
+++ precache-manifest.02979778be753391cde5626e574c1e3d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf6e3ca553eeb6c7005b12ea769b368a",
+    "url": "/react_todo-app/index.html"
+  },
+  {
+    "revision": "7387c744d125a9f304b4",
+    "url": "/react_todo-app/static/css/main.abae98c4.chunk.css"
+  },
+  {
+    "revision": "2c7235e190946364d8b9",
+    "url": "/react_todo-app/static/js/2.d4b443b1.chunk.js"
+  },
+  {
+    "revision": "7387c744d125a9f304b4",
+    "url": "/react_todo-app/static/js/main.f6d96b9d.chunk.js"
+  },
+  {
+    "revision": "51af3b3c9c5f8ccb1c53",
+    "url": "/react_todo-app/static/js/runtime~main.df364381.js"
+  }
+]);
